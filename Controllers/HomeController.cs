192afb9903ca43f3261f7a920c75065da5f015dd
Tim Logtenberg <tplogtenberg@gmail.com>
2021-02-17 13:42:04 +0100

    Show product name, price and supply in View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,9 +3,10 @@ using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using Festivalsite.Models;
 using MySql.Data;
+using System;
 using System.Collections.Generic;
 using MySql.Data.MySqlClient;
-using System;
+using Festivalsite.Database;
 
 namespace Festivalsite.Controllers
 {
@@ -21,15 +22,57 @@ namespace Festivalsite.Controllers
     public IActionResult Index()
     {
       // alle namen ophalen
-      var names = GetNames();
+      var products = GetProducts();
       
       // stop de namen in de html
-      return View(names);
+      return View(products);
     }
 
     public IActionResult Privacy()
     {
       return View();
+    }
+
+    public List<Product> GetProducts()
+    {
+      // stel in waar de database gevonden kan worden
+      string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=0P%Y9fI2GdO#;";
+
+      // maak een lege lijst waar we de namen in gaan opslaan
+      List<Product> products = new List<Product>();
+
+      // verbinding maken met de database
+      using (MySqlConnection conn = new MySqlConnection(connectionString))
+      {
+        // verbinding openen
+        conn.Open();
+
+        // SQL query die we willen uitvoeren
+        MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+        // resultaat van de query lezen
+        using (var reader = cmd.ExecuteReader())
+        {
+          // elke keer een regel (of eigenlijk: database rij) lezen
+          while (reader.Read())
+          {
+            Product p = new Product
+            {
+              // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+              Id = Convert.ToInt32(reader["Id"]),
+              Beschikbaarheid = Convert.ToInt32(reader["Beschikbaarheid"]),
+              Naam = reader["Naam"].ToString(),
+              Prijs = reader["Prijs"].ToString(),
+            };          
+
+            // voeg de naam toe aan de lijst met namen
+            products.Add(p);
+          }
+        }
+      }
+
+      // return de lijst met namen
+      return products;
     }
 
     public List<string> GetNames()
---
+++ Database/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Festivalsite.Database
+{
+  public class Product
+  {
+    public int Id { get; set; }
+
+    public string Naam { get; set; }
+
+    public string Prijs { get; set; }
+
+    public int Beschikbaarheid { get; set; }
+  }
+}
